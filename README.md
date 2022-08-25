01b2b0e11d2878e13b267f5979ec652af2e2e4a9
caot <charlietang98@gmail.com>
2022-08-25 11:47:44 -0400

    loading img

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ Download a copy of your Skype chat history and/or all available files - this inc
 
 https://secure.skype.com/en/data-export
 
-# The messages.json file is just a text file. You can view this file using any text editing tool, but for a readable view you would need a n JSON Viewer. You can find several JSON Viewers on the Net, but a version is also supplied by Microsoft.
+# skype-parser
+
+The messages.json file is just a text file. You can view this file using any text editing tool, but for a readable view you would need a n JSON Viewer. You can find several JSON Viewers on the Net, but a version is also supplied by Microsoft.
 
 https://go.skype.com/skype-parser
 
@@ -12,3 +14,6 @@ When you open this viewer (index.html), you will have to browser to the location
 
 https://answers.microsoft.com/en-us/skype/forum/all/how-do-i-parse-this-json-skype-history-thing/c5ade397-7f6c-4dce-bf03-af9dcf95a168
 
+# How do I export or delete my Skype data?
+
+https://support.skype.com/en/faq/FA34894/how-do-i-export-or-delete-my-skype-data
---
+++ index.js
@@ -0,0 +1,130 @@
+!function() {
+    "use strict";
+    function c(e) {
+        return e.displayName ? u(e.displayName) : p(e.id)
+    }
+    function m(e) {
+        e.preventDefault();
+        var t = parseInt(e.target.getAttribute("data-conv"), 10)
+          , n = document.getElementById("messages");
+        n.innerHtml = "",
+        n.textContent = "",
+        document.getElementById("selected-conversation-header").textContent = c(window.Skype.conversations[t]);
+        for (var a = window.Skype.conversations[t] && window.Skype.conversations[t].MessageList ? window.Skype.conversations[t].MessageList : [], s = 0; s < a.length; s++)
+            if ("" !== a[s].content) {
+                var r = document.createElement("li");
+                r.className = "message",
+                r.setAttribute("id", a[s].id);
+                var o = document.createElement("div");
+                r.appendChild(o);
+                var i = document.createElement("span");
+                i.className = "author",
+                i.textContent = p(a[s].from),
+                o.appendChild(i);
+                var l = document.createElement("span");
+                l.className = "timestamp",
+                l.textContent = new Date(a[s].originalarrivaltime).toLocaleString(),
+                o.appendChild(l);
+                var u_content = u(a[s].content);
+                var d = document.createElement("div");
+                d.className = "message-body";
+                if (typeof u_content == 'string') {
+                    d.innerText = u_content;
+                } else {
+                    r.appendChild(u_content);    
+                }
+                r.appendChild(d);
+                n.appendChild(r)
+            }
+    }
+    function a(e, t) {
+        document.getElementById(e).textContent = t
+    }
+    function p(e) {
+        return e.replace(/^8:/, "")
+    }
+    function u(e) {
+        var data = e.replace(/&apos;/g, "'").replace(/&amp;/g, "&").replace(/&lt;/g, "<").replace(/&gt;/g, ">")
+        if (data.startsWith('<URIObject uri')) {
+            var URIObject = (new DOMParser).parseFromString(e, "text/html").querySelector('[doc_id]');
+            // doc_id = (new DOMParser).parseFromString(e, "text/html").querySelector('[doc_id]').getAttribute('doc_id');
+            var doc_id = URIObject.getAttribute('doc_id');
+            var image = doc_id + '.1.png'
+            var width = URIObject.getAttribute('width');
+            var height = URIObject.getAttribute('height');
+            (new DOMParser).parseFromString(e, "text/html").querySelector('[doc_id]').getAttribute('width');
+
+            // var img = "<img src='" + image + "' width='" + width + "' height='" + height + "'>";
+
+            var img = document.createElement("img");
+            img.setAttribute("src", '../8 live .cid.930f95666ffeb1cb_export/media/' + image);
+            img.setAttribute("width", width/2);
+            img.setAttribute("height", height/2);
+
+            return img;
+        }
+        console.log(data);
+
+        return data;
+    }
+    function s(e, t) {
+        for (var n = document.getElementsByClassName(e), a = 0; a < n.length; a++)
+            n[a].style.display = t ? "block" : "none"
+    }
+    document.getElementById("btnLoad").addEventListener("click", function() {
+        var e, t, n;
+        "function" == typeof window.FileReader ? (e = document.getElementById("fileinput")) && e.files ? e.files[0] ? (document.getElementById("progress").style.display = "block",
+        t = e.files[0],
+        (n = new FileReader).onload = function(e) {
+            var t = e.target.result;
+            !function(e) {
+                if (e && e.userId && e.conversations && e.exportDate) {
+                    var t = new Date(e.exportDate)
+                      , n = e.conversations.filter(function(e) {
+                        return e.id && "ALL" !== e.id && !/@cast\.skype$/.test(e.id) && e.MessageList && e.MessageList.length
+                    });
+                    a("hdr-user", p(e.userId)),
+                    a("hdr-exported", t.toLocaleString()),
+                    a("hdr-stats", n.length + " conversations"),
+                    function(e) {
+                        for (var t = document.getElementById("conversations"), n = 0; n < e.length; n++) {
+                            var a = document.createElement("li");
+                            a.className = "conversations";
+                            var s = document.createElement("a");
+                            s.setAttribute("href", "#"),
+                            s.setAttribute("data-conv", n),
+                            s.className = "conv-link",
+                            s.textContent = c(e[n]),
+                            a.appendChild(s);
+                            var r = document.createElement("div");
+                            a.appendChild(r);
+                            var o = document.createElement("span");
+                            o.className = "messageCount",
+                            o.textContent = e[n].MessageList.length + " messages",
+                            r.appendChild(o);
+                            var i = e[n].properties && e[n].properties.lastimreceivedtime ? e[n].properties.lastimreceivedtime : null;
+                            if (i) {
+                                var l = document.createElement("span");
+                                l.className = "timestamp-conv",
+                                l.textContent = "Last from: " + new Date(i).toLocaleString(),
+                                r.appendChild(l)
+                            }
+                            t.appendChild(a)
+                        }
+                        for (var d = document.getElementsByClassName("conv-link"), n = 0; n < d.length; n++)
+                            d[n].addEventListener("click", m)
+                    }(n),
+                    window.Skype = {},
+                    window.Skype.conversations = n,
+                    s("step-1", !1),
+                    s("step-2", !0)
+                } else
+                    alert("Sorry, we are unable to load this file")
+            }(JSON.parse(t))
+        }
+        ,
+        n.readAsText(t, "utf8")) : alert("Please select a file before clicking 'Load'") : alert("This browser doesn't seem to support the 'files' property of file inputs.") : alert("The file API isn't supported on this browser. Please use a more modern browser.")
+    }),
+    s("step-2", !1),
+    s("step-1", !0)
+}();
